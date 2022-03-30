e51b0c4aed736bf14c771dd7fc65a592bd97a5b2
John Jacko <jljacko@outlook.com>
2022-03-29 21:14:47 -0500

    Added Exception handling to create user call

--- src/main/java/com/trecapps/users/services/UserService.java
+++ src/main/java/com/trecapps/users/services/UserService.java
@@ -3,6 +3,8 @@ package com.trecapps.users.services;
 import com.trecapps.users.models.PasswordChange;
 import com.trecapps.users.models.UserPost;
 import com.trecapps.users.security.TokenProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,9 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class UserService {
+
+
+    Logger logger = LoggerFactory.getLogger(UserService.class);
 
     RestTemplate graphClient;
 
@@ -40,24 +45,34 @@ public class UserService {
         MultiValueMap<String, String> authHeaders = new LinkedMultiValueMap<>(headers);
         authHeaders.add("Authorization", tokenProvider.getAuthToken());
 
-        switch(graphClient.exchange(baseUrl + "users", HttpMethod.POST, new HttpEntity<>(post, authHeaders), String.class).getStatusCode()) {
-            case CREATED:
-            case OK:
-            case NO_CONTENT:
-            case ACCEPTED:
-                return monotize(new ResponseEntity<String>("Success", HttpStatus.OK));
-            case UNAUTHORIZED:
-            case FORBIDDEN:
-                return monotize(new ResponseEntity<String>("Error Connecting to Azure Active Directory", HttpStatus.INTERNAL_SERVER_ERROR));
-            case NOT_FOUND:
-            case INTERNAL_SERVER_ERROR:
-            case BAD_GATEWAY:
-                return monotize(new ResponseEntity<String>("Error Connecting to Azure Active Directory", HttpStatus.BAD_GATEWAY));
-            case BAD_REQUEST:
-                return monotize(new ResponseEntity<String>("Error In your submission!", HttpStatus.BAD_REQUEST));
-            default:
-                return monotize(new ResponseEntity<String>("Unknown Error Occurred", HttpStatus.INTERNAL_SERVER_ERROR));
+        try
+        {
+            switch(graphClient.exchange(baseUrl + "users", HttpMethod.POST, new HttpEntity<>(post, authHeaders), String.class).getStatusCode()) {
+                case CREATED:
+                case OK:
+                case NO_CONTENT:
+                case ACCEPTED:
+                    return monotize(new ResponseEntity<String>("Success", HttpStatus.OK));
+                case UNAUTHORIZED:
+                case FORBIDDEN:
+                    return monotize(new ResponseEntity<String>("Error Connecting to Azure Active Directory", HttpStatus.INTERNAL_SERVER_ERROR));
+                case NOT_FOUND:
+                case INTERNAL_SERVER_ERROR:
+                case BAD_GATEWAY:
+                    return monotize(new ResponseEntity<String>("Error Connecting to Azure Active Directory", HttpStatus.BAD_GATEWAY));
+                case BAD_REQUEST:
+                    return monotize(new ResponseEntity<String>("Error In your submission!", HttpStatus.BAD_REQUEST));
+                default:
+                    return monotize(new ResponseEntity<String>("Unknown Error Occurred", HttpStatus.INTERNAL_SERVER_ERROR));
+            }
         }
+        catch( Exception e)
+        {
+            logger.error("Exception occurred", e);
+            return monotize(new ResponseEntity<String>("Unknown Error Occurred", HttpStatus.INTERNAL_SERVER_ERROR));
+        }
+
+
     }
 
     public ResponseEntity<String> updatePassword(PasswordChange change, String auth)
