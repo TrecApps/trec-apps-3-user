b82d63c70631c9ce0463a85fb48c4c8fdf889658
John Jacko <jljacko@outlook.com>
2022-06-29 21:50:45 -0500

    Tightened Security

--- src/main/java/com/trecapps/users/security/SecurityConfig.java
+++ src/main/java/com/trecapps/users/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.trecapps.users.security;
 
 import com.trecapps.auth.services.TrecAccountService;
+import com.trecapps.auth.services.TrecSecurityContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 @Configuration
@@ -15,13 +17,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    SecurityConfig(TrecAccountService trecAccountService1)
+    SecurityConfig(TrecAccountService trecAccountService1, TrecSecurityContext trecSecurityContext1)
     {
         //aadAuthProps.setRedirectUriTemplate("http://localhost:4200/api");
         trecAccountService = trecAccountService1;
-
+        trecSecurityContext = trecSecurityContext1;
     }
     TrecAccountService trecAccountService;
+    TrecSecurityContext trecSecurityContext;
 
     String[] restrictedEndpoints = {
             "/Users/passwordUpdate",
@@ -43,6 +46,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .userDetailsService(trecAccountService)
+                .securityContext().securityContextRepository(trecSecurityContext).and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ;
     }
 
